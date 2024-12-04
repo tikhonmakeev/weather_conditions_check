2217ebc979304da465582bead93cc8dc3e8d4b59
tikhonmakeev <tihon.makeev@yandex.ru>
2024-12-04 05:21:40 +0300

    Made api in class

--- api_worker.py
+++ api_worker.py
@@ -5,48 +5,53 @@ from dotenv import load_dotenv
 load_dotenv('config.env')
 
 
-def get_location_key(location):
-    """
-    Searching the AccuWeather location key for a given location name
+class Weather:
+    def __init__(self, location):
+        self.location = location
+        self.location_key = self.get_key()
 
-    Args:
-        location: The query string of the location (e.g., "London", "New York")
+    def get_key(self):
+        """
+        Searching the AccuWeather location key
 
-    Returns:
-        The AccuWeather location key (a string) if the location is found, 
-        or None if there is an error or the location is not found
-    """
-    search_url = 'http://dataservice.accuweather.com/locations/v1/search'
+        Args:
+            location: The query string of the location (e.g., "London", "New York")
 
-    params = {'q': location,
-              'apikey': accuweather_token}
-    response = requests.get(search_url, params=params)
-    try:
-        response.raise_for_status()
-        if response.json():
-            return response.json()[0]['Key']
-    except Exception as e:
-        print(f'Error: {e}')
-        return None
+        Returns:
+            The AccuWeather location key (a string) if the location is found, 
+            or None if there is an error or the location is not found
+        """
+        search_url = 'http://dataservice.accuweather.com/locations/v1/search'
 
+        params = {'q': self.location,
+                  'apikey': accuweather_token}
+        response = requests.get(search_url, params=params)
+        try:
+            response.raise_for_status()
+            if response.json():
+                return response.json()[0]['Key']
+        except Exception as e:
+            print(f'Error: {e}')
+            return None
 
-def get_weather_by_location_key(location_key):
-    weather_url = f'http://dataservice.accuweather.com/currentconditions/v1/{
-        location_key}'
-    params = {'apikey': accuweather_token}
-    response = requests.get(weather_url, params=params)
-    try:
-        response.raise_for_status()
-        weather_data = response.json()[0]
-        return {
-            'temperature': weather_data['Temperature']['Metric']['Value'],
-            'weather_text': weather_data['WeatherText']
-        }
-    except Exception as e:
-        print(f'Error: {e}')
-        return None
+    def get_current_weather(self):
+        weather_url = f'http://dataservice.accuweather.com/currentconditions/v1/{
+            self.location_key}'
+        params = {'apikey': accuweather_token}
+        response = requests.get(weather_url, params=params)
+        try:
+            response.raise_for_status()
+            weather_data = response.json()[0]
+            return {
+                'temperature': weather_data['Temperature']['Metric']['Value'],
+                'weather_text': weather_data['WeatherText']
+            }
+        except Exception as e:
+            print(f'Error: {e}')
+            return None
 
 
 if __name__ == '__main__':
     accuweather_token = os.getenv('accuweather_token')
-    print(get_weather_by_location_key(get_location_key('London')))
+    weather_loc = Weather('London')
+    print(weather_loc.get_current_weather())
