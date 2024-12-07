052dff97c0bacca89f3f2253881e2ccbdccaf3f2
tikhonmakeev <tihon.makeev@yandex.ru>
2024-12-07 20:46:23 +0300

    flask added processing logic

--- web_app.py
+++ web_app.py
@@ -10,5 +10,23 @@ def index():
     return render_template('route.html')
 
 
+@app.route('/', methods=['POST'])
+def index_post():
+    request_data = request.form
+    processed_data = process_data(request_data)
+    return jsonify(processed_data), 200
+
+
+def process_data(data):
+    city_start = Weather(data['startpoint'])
+    city_end = Weather(data['endpoint'])
+    start_weather = city_start.get_one_day_forecast()
+    end_weather = city_end.get_one_day_forecast()
+    return {'start_weather': start_weather,
+            'end_weather': end_weather,
+            'is_bad_weather_at_start': BadWeatherModel.is_weather_bad(start_weather),
+            'is_bad_weather_at_end': BadWeatherModel.is_weather_bad(end_weather)}
+
+
 if __name__ == '__main__':
     app.run(debug=True)
